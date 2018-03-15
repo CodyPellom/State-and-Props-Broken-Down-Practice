db3c41274709c874364bd4a9775256ea211382b6
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-15 12:56:00 -0400

    all components

--- state-props-practice/src/App.js
+++ state-props-practice/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Users from './components/Users'
+import Child from './child'
+
+
 class App extends Component {
 //Peter would run first
   //state = {
@@ -39,19 +41,58 @@ window width is BELOW 500, the condition is being met. Once this is met, the sta
 the VDOM what we want to alter the State with.  */
       this.setState({innerWidth:window.innerWidth})
 /* Next, we will console log Render. render occurs after constructor, and cWM, and it'b job is basically to render 
-the component. Render is going to take the state and the props and render our component. */
-    console.log('componentWillMount')
-  }
+the component. Render is going to take the state and the props and render our component */
+    }  
+console.log('componentWillMount')
+  
 }
+/* Initally we had cWM, but AFTER render its cDM. THis means it has already rendered. What can we do with cDM? We can make
+AJAX calls here, this is the best place to o it, or to setup any subscriptions. However you must unsubsribe when the component 
+is Unmounted. You could technically setState in cDM, however it will re-render the component if you choose to setState in cDM.   */
+componentDidMount(){
+//   In a hiarchy of components, the last ones
+// go first, and then works up the chain. The app's component, componentDidMount, will run in 
+// the end. ComponentDidMount is only run once.
+  console.log('componentDidMount')
 
+}
+//Another lifecyce hook in our tutorial, when we do the same to the child, we hook on another lifecycle to the 
+//button associated with "changeState"
+componentWillReceiveProps(){
+  console.log('componentWillReceiveProps')
+}
+//This sounds more like a question, but it is the decision over should we re-render the component or not
+//with return true, or return false. sCU also, has two arguments which are nextProps, and nextState
+
+shouldComponentUpdate(nextProps, nextState){
+  console.log('shouldComponentUpdate')
+  return true;
+}
+//cWU is similiar to componentWillMount when you first init the render. if you want to set some variable based on
+//the state, you cando it here because you have state and prop availiable. However, you cannot run a setState here, 
+//because it will cause an infinite loop by creating another componentWillUpdate over and over ad infinum. 
+componentWillUpdate() {
+  console.log('componentWillUpdate')
+}
+//Change state is being called below in the button function. This is the function itself which will contain the change to the page
+//and alter the state itself. 
+changeState() {
+  this.setState({name: 'jill'})
+}
   render() {
     return (
       <div className="App">
       Name: {this.state.name}
 {/* This is "calling" the actual change. This is where the change itself will appear, if the condition is met! */}
    <br/> | <br/> innerWidth: {this.state.innerWidth}
-      
-        <Users/>
+{/*Inserting/Teleporting the child from ./child; now whatever state we set up in ./child will be teleported into app. 
+Don't forget to import the "Child" component at the top of App.js, as seen above...  */}
+      <Child name={this.state.name}/>
+{/*Here, I'm making a button that will alter the VitrualDOM with onClick. changeState above will dictate what effect the 
+button will have on the page & what changes will happen to the State! And when applying a change in this button, we will also
+include a .bind(this). */}
+      <button onClick={this.changeState.bind(this)}>Change State</button>
+        {/* Uncomment this out before tutorial! <Users/> */}
       </div>
     );
   }
---
+++ state-props-practice/src/child.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Users from './components/Users'
+
+
+class Child extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+name: "John"
+    
+    }
+    console.log('constructor')
+  }
+
+  componentWillMount(){
+console.log('child componentWillMount')
+  
+}
+
+componentDidMount(){
+  console.log('child componentDidMount')
+
+}
+
+componentWillReceiveProps(){
+    console.log('child componentWillReceiveProps')
+  }
+//if you return false, component will not re-render and will only change the parent coponent. the child component
+//will not be altered. By returning true, the re-render is allowed, and John will be changed to Jill. 
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('child shouldComponentUpdate')
+    return true;
+  }
+
+  componentWillUpdate() {
+    console.log('child componentWillUpdate')
+  }
+
+  render() {
+      console.log('child render')
+    return (
+      <div className="App">
+    {/* this will run before the App's cDM. In a hiarchy of components, the last ones
+go first, and then works up the chain. The app's component, componentDidMount, will run in 
+the end. ComponentDidMount is only run once.  */}
+        child name: {this.props.name}
+      </div>
+    );
+  }
+}
+
+export default Child;
